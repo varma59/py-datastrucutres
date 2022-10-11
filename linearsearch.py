5ecd2a2075a93add15097f476c177dfa328eaf6d
Abhishek Varma G V S <75376880+varma59@users.noreply.github.com>
2022-10-11 10:58:50 +0530

    Create linearsearch.py

---
+++ linearsearch.py
@@ -0,0 +1,16 @@
+def linear(list,n,key):
+    for i in range(0,n):
+        if(list[i]==key):
+            return i;
+            return -1;
+ # now we input the list        
+list1 = [10,20,30,80,90,96]
+n1 = len(list1)
+key1 = 96
+# function call
+res = linear(list1,n1,key1)
+#printing the location of value
+if(res == -1):
+    print("elemnt not found")
+else:
+    print("element found ",res)
